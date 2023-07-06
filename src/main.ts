6ef1e1bcf19fa06ef3a2a5e72dbb5224a502f064
Kaio Lino <kaiomudkt@gmail.com>
2023-07-05 22:42:01 -0400

    fix: correção env

--- src/main.ts
+++ src/main.ts
@@ -4,22 +4,27 @@ import { ValidationPipe } from '@nestjs/common';
 import { AllExceptionFilter } from './infra/common/middlewares/exception.filter';
 import { LoggerService } from './infra/utils/logger/logger.service';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
-import * as dotenv from 'dotenv';
+import { config } from 'dotenv';
 
 async function bootstrap() {
+  config();
   const app = await NestFactory.create(AppModule);
-  const config = new DocumentBuilder()
+  const swaggerConfig = new DocumentBuilder()
     .setTitle('Sistema de cargos e permissões')
     .setDescription('Usuario possuem cargos e permissoes')
-    .setVersion(process.env.BACKEND_API_VERSION || 'v1')
+    .setVersion(process.env.BACKEND_API_VERSION || '1.0')
     .addTag('usuarios')
     .build();
-  const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup(process.env.BACKEND_SWAGGER, app, document);
+  const document = SwaggerModule.createDocument(app, swaggerConfig);
+  SwaggerModule.setup(
+    process.env.BACKEND_API_SWAGGER || 'swagger-api',
+    app,
+    document,
+  );
   app.enableCors();
   app.useGlobalPipes(new ValidationPipe());
   app.useGlobalFilters(new AllExceptionFilter(new LoggerService()));
-  const port = process.env.BACKEND_PORT || 3000;
+  const port = process.env.BACKEND_API_PORT || 3000;
   await app.listen(port);
 }
 
